d2ffeb2432b126f6a513fd726c464da676f6bd80
getinos <sujaydas0325@gmail.com>
2025-02-10 22:18:22 +0530

    a

--- index.php
+++ index.php
@@ -14,10 +14,7 @@
             <div id="leaderboard-list">
                 <?php include 'Backend/leaderboard.php'; ?>
             </div>
-            <!-- <div class="bidding-history-container">
-                <h2>📜 Bidding History</h2>
-                <div id="bidding-history" class="bidding-history-box"></div>
-            </div> -->
+            <!--  -->
         </div>
        
         
@@ -47,10 +44,14 @@
         </div>
 
         <!-- Right: Team List -->
-        <div class="team-list">
+       <!-- <div class="team-list">
             <h2>Team Squads</h2>
             <div id="team-list-container"></div>
-        </div>
+        </div>-->
+        <div class="bidding-history-container">
+                <h2>📜 Bidding History</h2>
+                <div id="bidding-history" class="bidding-history-box"></div>
+            </div>
     </div>
 
     <script src="script.js"></script>
